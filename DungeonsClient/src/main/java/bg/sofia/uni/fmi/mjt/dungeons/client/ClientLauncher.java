01156c42f9ea5938863ec8fcfe8a5015216b03e4
Borislav-K <53512843+Borislav-K@users.noreply.github.com>
2020-12-21 16:07:06 +0200

    Create subprojects for client and server (#1)
    * Split project into 2 modules - client&server
    
    * Add dummy implementations for client&server
    
    * Add bootstrap instructions

---
+++ DungeonsClient/src/main/java/bg/sofia/uni/fmi/mjt/dungeons/client/ClientLauncher.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.dungeons.client;
+
+public class ClientLauncher {
+    public static void main(String[] args) {
+        System.out.println("Starting a new client!");
+    }
+}
---
+++ DungeonsServer/src/main/java/bg/sofia/uni/fmi/mjt/dungeons/server/ServerLauncher.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.dungeons.server;
+
+public class ServerLauncher {
+    public static void main(String[] args) {
+        System.out.println("Starting a new server!");
+    }
+}
