e26039eb92384e45f76bf8bfc3b85b5742737f5b
Bobi <bobissss84@gmail.com>
2021-01-30 19:59:20 +0200

    Add tests for smartbuffer

---
+++ DungeonsLibrary/src/test/java/bg/sofia/uni/fmi/mjt/dungeons/lib/network/SmartBufferTest.java
@@ -0,0 +1,29 @@
+package bg.sofia.uni.fmi.mjt.dungeons.lib.network;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SmartBufferTest {
+
+    private static final int TEST_BUFFER_CAPACITY = 128;
+
+    private static SmartBuffer testBuffer;
+
+    @Before
+    public void setUp() {
+        testBuffer = new SmartBuffer(TEST_BUFFER_CAPACITY);
+    }
+
+    @Test
+    public void testStringReadingAndWriting() {
+        String testString = "I have some non-english characters here асдф";
+        testBuffer.write(testString);
+        byte[] bytes = testBuffer.read();
+        assertArrayEquals(testString.getBytes(StandardCharsets.UTF_8), bytes);
+    }
+
+}
