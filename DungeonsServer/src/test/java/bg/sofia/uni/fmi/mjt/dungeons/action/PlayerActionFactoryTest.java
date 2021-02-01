af34cdd6af1645fd64d2b07fae89c3877baa6b07
Bobi <bobissss84@gmail.com>
2021-02-01 13:23:02 +0200

    wip

--- DungeonsServer/src/test/java/bg/sofia/uni/fmi/mjt/dungeons/action/PlayerActionFactoryTest.java
+++ DungeonsServer/src/test/java/bg/sofia/uni/fmi/mjt/dungeons/action/PlayerActionFactoryTest.java
@@ -1,13 +1,82 @@
 package bg.sofia.uni.fmi.mjt.dungeons.action;
 
+import bg.sofia.uni.fmi.mjt.dungeons.enums.ActionType;
+import bg.sofia.uni.fmi.mjt.dungeons.enums.Direction;
+import bg.sofia.uni.fmi.mjt.dungeons.exceptions.IllegalPlayerActionException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+import java.nio.channels.SocketChannel;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PlayerActionFactoryTest {
 
+    @Mock
+    private SocketChannel dummyChannel;
+
+    @Test(expected = IllegalPlayerActionException.class)
+    public void testIllegalCommandInputCreation() throws IllegalPlayerActionException {
+        PlayerActionFactory.create("ILLEGAL", dummyChannel);
+    }
 
     @Test
-    public void testSth() {
+    public void testMoveUpCommandCreation() throws IllegalPlayerActionException {
+        PlayerAction createdAction = PlayerActionFactory.create("mvu", dummyChannel);
+        assertMovementAction(createdAction, Direction.UP);
+    }
 
+    @Test
+    public void testMoveDownCommandCreation() throws IllegalPlayerActionException {
+        PlayerAction createdAction = PlayerActionFactory.create("mvd", dummyChannel);
+        assertMovementAction(createdAction, Direction.DOWN);
+    }
+
+    @Test
+    public void testMoveLeftCommandCreation() throws IllegalPlayerActionException {
+        PlayerAction createdAction = PlayerActionFactory.create("mvl", dummyChannel);
+        assertMovementAction(createdAction, Direction.LEFT);
+    }
+
+    @Test
+    public void testMoveRightCommandCreation() throws IllegalPlayerActionException {
+        PlayerAction createdAction = PlayerActionFactory.create("mvr", dummyChannel);
+        assertMovementAction(createdAction, Direction.RIGHT);
+    }
+
+    @Test
+    public void testAttackCommandCreation() throws IllegalPlayerActionException {
+        PlayerAction createdAction = PlayerActionFactory.create("att", dummyChannel);
+        assertEquals(ActionType.ATTACK, createdAction.type());
+    }
+
+    @Test
+    public void testTreasurePickupCommandCreation() throws IllegalPlayerActionException {
+        PlayerAction createdAction = PlayerActionFactory.create("pck", dummyChannel);
+        assertEquals(ActionType.TREASURE_PICKUP, createdAction.type());
+    }
+
+    @Test
+    public void testItemUsageCommandCreationWhenItemNumberIsInBounds() throws IllegalPlayerActionException {
+        PlayerAction createdAction = PlayerActionFactory.create("us1", dummyChannel);
+        assertEquals(ActionType.ITEM_USAGE, createdAction.type());
+        ItemUsage itemUsage = (ItemUsage) createdAction;
+        assertEquals(1, itemUsage.itemNumber());
+    }
+
+    @Test(expected = IllegalPlayerActionException.class)
+    public void testItemUsageCommandCreationWhenItemNumberIsOutOfBounds() throws IllegalPlayerActionException {
+        PlayerActionFactory.create("us10", dummyChannel);
+    }
+
+
+    private static void assertMovementAction(PlayerAction action, Direction direction) {
+        assertEquals(ActionType.MOVEMENT, action.type());
+        PlayerMovement playerMovement = (PlayerMovement) action;
+        assertEquals(direction, playerMovement.direction());
     }
 
 }
