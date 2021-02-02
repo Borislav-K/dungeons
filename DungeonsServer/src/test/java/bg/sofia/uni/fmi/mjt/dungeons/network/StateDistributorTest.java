22ae02eb2f35ad029e2ad4a9805b3d6233e71663
Bobi <bobissss84@gmail.com>
2021-02-02 13:00:29 +0200

    Add 1 more test for state distr

--- DungeonsServer/src/test/java/bg/sofia/uni/fmi/mjt/dungeons/network/StateDistributorTest.java
+++ DungeonsServer/src/test/java/bg/sofia/uni/fmi/mjt/dungeons/network/StateDistributorTest.java
@@ -3,6 +3,7 @@ package bg.sofia.uni.fmi.mjt.dungeons.network;
 import bg.sofia.uni.fmi.mjt.dungeons.GameMap;
 import bg.sofia.uni.fmi.mjt.dungeons.PlayerManager;
 import bg.sofia.uni.fmi.mjt.dungeons.io.PerformantByteArrayOutputStream;
+import bg.sofia.uni.fmi.mjt.dungeons.lib.Position2D;
 import bg.sofia.uni.fmi.mjt.dungeons.lib.actors.Player;
 import bg.sofia.uni.fmi.mjt.dungeons.lib.network.PlayerSegment;
 import org.junit.Before;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.DataOutputStream;
@@ -29,18 +31,30 @@ public class StateDistributorTest {
     private SocketChannel playerChannel;
     private PlayerSegment playerSegment;
 
-    private PlayerManager playerManager = new PlayerManager();
-    private GameMap gameMap = new GameMap();
+    @Spy
+    private PlayerManager playerManager = spy(new PlayerManager());
+    @Spy
+    private GameMap gameMap = spy(new GameMap());
+
     private StateDistributor testStateDistributor = new StateDistributor(playerManager, gameMap);
 
     @Before
     public void setUp() throws Exception {
         Player player = playerManager.createNewPlayer(playerChannel);
+        player.setPosition(new Position2D(10,10));
         playerSegment = new PlayerSegment(player, gameMap.getPositionsWithActors());
     }
 
     @Test
-    public void testStateDistribution() throws IOException {
+    public void testStateDistributionWhenPlayerDisconnected() throws IOException {
+        when(playerChannel.write(any(ByteBuffer.class))).thenThrow(IOException.class);
+        testStateDistributor.distributeState();
+        verify(playerManager).removePlayer(any());
+        verify(gameMap).despawnActor(any(Player.class));
+    }
+
+    @Test
+    public void testStateDistributionOK() throws IOException {
         testStateDistributor.distributeState();
         byte[] playerSegmentBytes = serializePlayerSegment();
 
